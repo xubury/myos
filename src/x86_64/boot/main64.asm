27de74f367583bb08d84fec8f4704c7c8b125ddb
xubury <xubury97@gmail.com>
2021-02-27 11:51:23 +0800

    * delete useless code

--- src/x86_64/boot/main64.asm
+++ src/x86_64/boot/main64.asm
@@ -4,15 +4,5 @@ extern kernelStart
 section .text
 bits 64
 long_mode_start:
-	;load null into all data segment registers
-	mov ax, 0
-	mov ss, ax
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-
-
 	call kernelStart
-
 	hlt
