13efc6d097e769ac95e0385cc6de69ad4abfbc54
xubury <xubury97@gmail.com>
2021-03-06 17:56:57 +0800

    * optimize keyboard handler

--- src/keyboard/Keyboard.cpp
+++ src/keyboard/Keyboard.cpp
@@ -1,14 +1,33 @@
 #include "Keyboard.hpp"
 
+bool isLeftShiftPressed;
+bool isRightShiftPressed;
 void handleKeyboard(uint8_t scanCode) {
-    if (scanCode == QWERTYKeyboard::BackSpace) {
-        manager().renderer().backSapce(1);
-    } else if (scanCode == QWERTYKeyboard::Enter) {
-        manager().renderer().newLine();
-    } else {
-        char ascii = QWERTYKeyboard::translate(scanCode, false);
-        if (ascii != 0) {
-            manager().renderer().putChar(ascii);
-        }
+    switch (scanCode) {
+        case QWERTYKeyboard::BackSpace:
+            manager().renderer().backSapce(1);
+            break;
+        case QWERTYKeyboard::Enter:
+            manager().renderer().newLine();
+            break;
+        case QWERTYKeyboard::LeftShift:
+            isLeftShiftPressed = true;
+            break;
+        case QWERTYKeyboard::RightShift:
+            isLeftShiftPressed = true;
+            break;
+        case QWERTYKeyboard::LeftShift + 0x80:
+            isLeftShiftPressed = false;
+            break;
+        case QWERTYKeyboard::RightShift + 0x80:
+            isLeftShiftPressed = false;
+            break;
+        default:
+            char ascii = QWERTYKeyboard::translate(
+                scanCode, isLeftShiftPressed || isRightShiftPressed);
+            if (ascii != 0) {
+                manager().renderer().putChar(ascii);
+            }
+            break;
     }
 }
